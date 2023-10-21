a97741f498c3f42bee837aac7494c321a5c8ac8f
Heliandro Lavor <heliandro.lavor@ciandt.com>
2023-10-21 17:53:35 -0300

    refactor(metrics-service): converte a response do datasource para object

--- metrics-service/src/providers/metrics-config/metrics-config.provider.ts
+++ metrics-service/src/providers/metrics-config/metrics-config.provider.ts
@@ -3,5 +3,5 @@ import {IDataSource} from "../../shared/common.types";
 
 export async function getDatasources(): Promise<IDataSource[]> {
   const res = await axios.get<IDataSource[]>(process.env.METRICS_CONFIG! + '/api/v1/metrics-config', {});
-  return res.data;
+  return res.data.map((ds: IDataSource) => ({...ds, meta: JSON.parse(ds.meta) }));
 }
